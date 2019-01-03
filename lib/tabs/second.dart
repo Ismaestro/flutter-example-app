bd3096451f0440ac4144d073de28bb5563d7f586
iramos <isma@metastartup.com>
2019-01-03 09:22:53 +0100

    First version of top heroes

--- lib/tabs/second.dart
+++ lib/tabs/second.dart
@@ -4,6 +4,85 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class SecondTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Column buildButtonColumn(IconData icon, String label) {
+      Color color = Theme.of(context).primaryColor;
+
+      return Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, color: color),
+          Container(
+            margin: const EdgeInsets.only(top: 8.0),
+            child: Text(
+              label,
+              style: TextStyle(
+                fontSize: 12.0,
+                fontWeight: FontWeight.w400,
+                color: color,
+              ),
+            ),
+          ),
+        ],
+      );
+    }
+
+    Widget buttonSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          buildButtonColumn(Icons.call, 'CALL'),
+          buildButtonColumn(Icons.near_me, 'ROUTE'),
+          buildButtonColumn(Icons.share, 'SHARE'),
+        ],
+      ),
+    );
+
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32.0),
+      child: Text(
+        '''
+Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes. A gondola ride from Kandersteg, followed by a half-hour walk through pastures and pine forest, leads you to the lake, which warms to 20 degrees Celsius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run.
+        ''',
+        softWrap: true,
+      ),
+    );
+
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32.0),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: Text(
+                    'Oeschinen Lake Campground',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Kandersteg, Switzerland',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          Text('41'),
+        ],
+      ),
+    );
+
     return StreamBuilder<QuerySnapshot>(
       stream: Firestore.instance
           .collection('heroes')
@@ -16,14 +95,34 @@ class SecondTab extends StatelessWidget {
           case ConnectionState.waiting:
             return new Text('Loading...');
           default:
-            return new ListView(
-              children:
-                  snapshot.data.documents.map((DocumentSnapshot document) {
-                return new ListTile(
-                  title: new Text(document['name']),
-                  subtitle: new Text(document['likes'].toString()),
-                );
-              }).toList(),
+            var firstElement = snapshot.data.documents[0];
+            var secondElement = snapshot.data.documents[1];
+            var thirdElement = snapshot.data.documents[2];
+            var fourthElement = snapshot.data.documents[3];
+
+            return ListView(
+              children: [
+                Image.network(firstElement['avatarUrl'],
+                    width: 600.0, height: 240.0, fit: BoxFit.cover),
+                titleSection,
+                buttonSection,
+                textSection,
+                Image.network(secondElement['avatarUrl'],
+                    width: 600.0, height: 240.0, fit: BoxFit.cover),
+                titleSection,
+                buttonSection,
+                textSection,
+                Image.network(thirdElement['avatarUrl'],
+                    width: 600.0, height: 240.0, fit: BoxFit.cover),
+                titleSection,
+                buttonSection,
+                textSection,
+                Image.network(fourthElement['avatarUrl'],
+                    width: 600.0, height: 240.0, fit: BoxFit.cover),
+                titleSection,
+                buttonSection,
+                textSection,
+              ],
             );
         }
       },
